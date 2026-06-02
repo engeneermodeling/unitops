a5ae37f8f65f45da6f53245409f53eb8fd4b2fce
TarasMysiuraTutor <tarasmysiuratutor@gmail.com>
2026-06-03 00:35:14 +0300

    Add Google verification to root layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,17 @@
 // src/app/layout.jsx
 export default function RootLayout({ children }) {
   return (
-    <html >
-    <meta name="google-site-verification" content="f5nalxq8R9B9N98asYtQaqbeD03S0T-mX9BgLfKKYe4" />
+    <html>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="f5nalxq8R9B9N98asYtQaqbeD03S0T-mX9BgLfKKYe4"
+        />
+      </head>
+      <meta
+        name="google-site-verification"
+        content="f5nalxq8R9B9N98asYtQaqbeD03S0T-mX9BgLfKKYe4"
+      />
       <body>{children}</body>
     </html>
   );
